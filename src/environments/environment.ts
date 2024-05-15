eb136d6097fb465d0673fe1de6454359dc765a81
afk98 <rcporshe@gmail.com>
2024-05-15 15:04:11 +0530

    local changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCp-PcnG-HPcrQcfMWdN5IVONLXIq5Ufwo',
+    authDomain: 'platformapp-afk98.firebaseapp.com',
+    projectId: 'platformapp-afk98',
+    storageBucket: 'platformapp-afk98.appspot.com',
+    messagingSenderId: '905024805372',
+    appId: '1:905024805372:web:d78a06edaaa443fa90fa79',
+    measurementId: 'G-2EW38YJTCL'
+  }
+};
